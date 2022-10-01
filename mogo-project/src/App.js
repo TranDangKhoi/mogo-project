6d6e698529caedf227c6e83299e153a9de1e67a1
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-10-01 16:12:27 +0700

    :+1: Set up wrapper and container width

--- mogo-project/src/App.js
+++ mogo-project/src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iure fuga
+      blanditiis sint, deserunt doloribus? Animi veniam atque, possimus facere
+      cum eligendi nihil. Eos, voluptate rem sunt aut ducimus commodi.
+    </div>
+  );
 }
 
 export default App;
